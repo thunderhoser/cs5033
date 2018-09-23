04a17a7d42d25ebede7b2511623df45a20e10b9c
thunderhoser <ryan.lagerquist@ou.edu>
2018-09-22 19:17:23 -0500

    Cleaned up linlog_regression_test.py.

--- linlog_regression_test.py
+++ linlog_regression_test.py
@@ -1,15 +1,15 @@
-"""Simple experiments with linear or logistic regression.
+"""Simple experiment with linear or logistic regression.
 
 This script has only one predictor variable (x) and one target variable (y).
 """
 
+import os
+import errno
 import argparse
 import numpy
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as pyplot
-
-# TODO(thunderhoser): Modularize and make titles nicer.
 
 SEPARATOR_STRING = '\n\n' + '*' * 50 + '\n\n'
 
@@ -18,18 +18,20 @@ X_MAXIMUM = 10.
 INITIAL_WEIGHT = 0.
 NUM_TRAINING_POINTS = 1000
 NUM_TESTING_POINTS = 1000
+DEFAULT_NOISE_STDEV_FOR_LINEAR = 5.
+DEFAULT_NOISE_STDEV_FOR_LOGISTIC = 0.2
 
 FIGURE_WIDTH_INCHES = 15
 FIGURE_HEIGHT_INCHES = 15
-DOTS_PER_INCH = 300
+FIGURE_RESOLUTION_DPI = 600
 
-MARKER_COLOUR = numpy.array([141, 160, 203], dtype=float) / 255
+LINE_WIDTH = 4
+LINE_COLOUR = numpy.array([252, 141, 98], dtype=float) / 255
+
 MARKER_TYPE = 'o'
 MARKER_SIZE = 8
 MARKER_EDGE_WIDTH = 1
-
-LINE_COLOUR = numpy.array([252, 141, 98], dtype=float) / 255
-LINE_WIDTH = 4
+MARKER_COLOUR = numpy.array([141, 160, 203], dtype=float) / 255
 
 FONT_SIZE = 25
 pyplot.rc('font', size=FONT_SIZE)
@@ -51,26 +53,32 @@ OUTPUT_DIR_ARG_NAME = 'output_dir_name'
 TRUE_SLOPE_HELP_STRING = (
     'True slope (dy/dx).  This will be used to construct the training and '
     'testing sets.')
+
 TRUE_INTERCEPT_HELP_STRING = (
     'True intercept (y at x = 0).  This will be used to construct the training '
     'and testing sets.')
+
 NOISE_STDEV_HELP_STRING = (
     'Standard deviation of Gaussian noise.  This will be added to y when '
     'constructing the training and testing sets.')
+
 NUM_ITERATIONS_HELP_STRING = (
-    'Number of iterations for linear regression.  This is the number of times '
-    'that the weights will be updated.')
+    'Number of iterations.  This is the number of times that the weights will '
+    'be updated.')
+
 LEARNING_RATE_HELP_STRING = (
-    'Learning rate for linear regression.  This is alpha in the weight-update '
-    'rule.')
+    'Learning rate.  This is alpha in the weight-update rule.')
+
 LOGISTIC_HELP_STRING = (
     'Boolean flag (0 or 1).  If 1, this script will do logistic regression.  If'
     ' 0, will do linear regression.')
+
 OUTPUT_DIR_HELP_STRING = (
     'Path to output directory.  Figures will be saved here.')
 
 DEFAULT_OUTPUT_DIR_NAME = (
-    '/home/ralager/Downloads/cs5033_fall2018/linear_regression_test')
+    '/home/ryan.lagerquist/Downloads/classes/cs5033_fall2018/'
+    'linlog_regression_test')
 
 INPUT_ARG_PARSER = argparse.ArgumentParser()
 INPUT_ARG_PARSER.add_argument(
@@ -82,24 +90,61 @@ INPUT_ARG_PARSER.add_argument(
     help=TRUE_INTERCEPT_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
-    '--' + NOISE_STDEV_ARG_NAME, type=float, required=False, default=0.2,
+    '--' + NOISE_STDEV_ARG_NAME, type=float, required=False, default=-1,
     help=NOISE_STDEV_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
-    '--' + NUM_ITERATIONS_ARG_NAME, type=int, required=False, default=1000,
+    '--' + NUM_ITERATIONS_ARG_NAME, type=int, required=False, default=10000,
     help=NUM_ITERATIONS_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
-    '--' + LEARNING_RATE_ARG_NAME, type=float, required=False, default=0.01,
+    '--' + LEARNING_RATE_ARG_NAME, type=float, required=False, default=0.001,
     help=LEARNING_RATE_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
-    '--' + LOGISTIC_ARG_NAME, type=int, required=False, default=1,
+    '--' + LOGISTIC_ARG_NAME, type=int, required=False, default=0,
     help=LOGISTIC_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
     '--' + OUTPUT_DIR_ARG_NAME, type=str, required=False,
     default=DEFAULT_OUTPUT_DIR_NAME, help=OUTPUT_DIR_HELP_STRING)
+
+
+def _inverse_logit(input_values):
+    """Implements the inverse logit function, defined below.
+
+    f(x) = exp(x) / [1 + exp(x)]
+
+    :param input_values: numpy array of logits.
+    :return: output_values: equivalent-size numpy of non-logits.
+    """
+
+    return numpy.exp(input_values) / (1 + numpy.exp(input_values))
+
+
+def _binary_xentropy(actual_values, predicted_values):
+    """Computes binary cross-entropy.
+
+    :param actual_values: numpy array of actual values.
+    :param predicted_values: equivalent-size numpy array of predicted values.
+    :return: cross_entropy: Binary cross-entropy.
+    """
+
+    return -numpy.mean(
+        actual_values * numpy.log2(predicted_values) +
+        (1. - actual_values) * numpy.log2(1. - predicted_values)
+    )
+
+
+def _mean_squared_error(actual_values, predicted_values):
+    """Computes mean squared error.
+
+    :param actual_values: numpy array of actual values.
+    :param predicted_values: equivalent-size numpy array of predicted values.
+    :return: mean_squared_error: Mean squared error.
+    """
+
+    return numpy.mean((predicted_values - actual_values) ** 2)
 
 
 def _generate_data(
@@ -125,7 +170,7 @@ def _generate_data(
     y_values = true_intercept + true_slope * x_values
 
     if do_logistic:
-        y_values = numpy.exp(y_values) / (1 + numpy.exp(y_values))
+        y_values = _inverse_logit(y_values)
         y_values = numpy.round(y_values + noise_values)
         y_values[y_values < 0] = 0
         y_values[y_values > 1] = 1
@@ -155,18 +200,13 @@ def _train_model(
     for i in range(num_iterations):
         predicted_y_values = weights[0] + weights[1] * x_values
 
-        # The "error" is either mean squared error or cross-entropy.
         if do_logistic:
-            predicted_y_values = (
-                numpy.exp(predicted_y_values) /
-                (1 + numpy.exp(predicted_y_values))
-            )
-            error = -numpy.mean(
-                y_values * numpy.log2(predicted_y_values) +
-                (1. - y_values) * numpy.log2(1. - predicted_y_values)
-            )
+            predicted_y_values = _inverse_logit(predicted_y_values)
+            error = _binary_xentropy(
+                actual_values=y_values, predicted_values=predicted_y_values)
         else:
-            error = numpy.mean((predicted_y_values - y_values) ** 2)
+            error = _mean_squared_error(
+                actual_values=y_values, predicted_values=predicted_y_values)
 
         first_gradient = 2 * numpy.mean(predicted_y_values - y_values)
         second_gradient = 2 * numpy.mean(
@@ -185,13 +225,14 @@ def _train_model(
 
 def _plot_results(x_values, y_values, weights, do_logistic, title_string,
                   output_file_name):
-    """Plots results of linear regression.
+    """Plots results of linear or logistic regression.
 
     N = number of examples
 
     :param x_values: length-N numpy array of x-values (predictor values).
     :param y_values: length-N numpy array of y-values (actual target values).
-    :param weights: Array produced by `_train_linear_regression`.
+    :param weights: length-2 numpy array, where weights[0] is the bias (beta_0)
+        and weights[1] is the slope (beta_1).
     :param do_logistic: See documentation at top of file.
     :param title_string: Figure title.
     :param output_file_name: Path to output file (the figure will be saved
@@ -208,27 +249,29 @@ def _plot_results(x_values, y_values, weights, do_logistic, title_string,
 
     if do_logistic:
         x_values_in_line = numpy.linspace(X_MINIMUM, X_MAXIMUM, num=1000)
-        y_values_in_line = weights[0] + weights[1] * x_values_in_line
-        y_values_in_line = (
-            numpy.exp(y_values_in_line) / (1 + numpy.exp(y_values_in_line))
-        )
     else:
         x_values_in_line = numpy.array([X_MINIMUM, X_MAXIMUM])
-        y_values_in_line = weights[0] + weights[1] * x_values_in_line
+
+    y_values_in_line = weights[0] + weights[1] * x_values_in_line
+    if do_logistic:
+        y_values_in_line = _inverse_logit(y_values_in_line)
 
     axes_object.plot(
         x_values_in_line, y_values_in_line, color=LINE_COLOUR,
         linewidth=LINE_WIDTH)
 
+    pyplot.xlabel(r'Predictor ($x$)')
+    pyplot.ylabel(r'Target ($y$)')
+
     pyplot.title(title_string)
     print 'Saving figure to: "{0:s}"...'.format(output_file_name)
-    pyplot.savefig(output_file_name, dpi=DOTS_PER_INCH)
+    pyplot.savefig(output_file_name, dpi=FIGURE_RESOLUTION_DPI)
     pyplot.close()
 
 
 def _run(true_slope, true_intercept, noise_standard_deviation, num_iterations,
          learning_rate, do_logistic, output_dir_name):
-    """Simple experiments with linear regression.
+    """Simple experiment with linear or logistic regression.
 
     This is effectively the main method.
 
@@ -241,6 +284,21 @@ def _run(true_slope, true_intercept, noise_standard_deviation, num_iterations,
     :param output_dir_name: Same.
     """
 
+    try:
+        os.makedirs(output_dir_name)
+    except OSError as this_error:
+        if this_error.errno == errno.EEXIST and os.path.isdir(output_dir_name):
+            pass
+        else:
+            raise
+
+    # If noise standard deviation <= 0, set to default.
+    if noise_standard_deviation <= 0:
+        if do_logistic:
+            noise_standard_deviation = DEFAULT_NOISE_STDEV_FOR_LOGISTIC + 0.
+        else:
+            noise_standard_deviation = DEFAULT_NOISE_STDEV_FOR_LINEAR + 0.
+
     training_x_values, training_y_values = _generate_data(
         num_points=NUM_TRAINING_POINTS, true_slope=true_slope,
         true_intercept=true_intercept,
@@ -251,33 +309,29 @@ def _run(true_slope, true_intercept, noise_standard_deviation, num_iterations,
         x_values=training_x_values, y_values=training_y_values,
         num_iterations=num_iterations, learning_rate=learning_rate,
         do_logistic=do_logistic)
-
     print SEPARATOR_STRING
 
     predicted_training_y_values = weights[0] + weights[1] * training_x_values
+    base_title_string = r'Iterations = {0:d} ... $\alpha$ = {1:.4e}'.format(
+        num_iterations, learning_rate)
+
     if do_logistic:
-        predicted_training_y_values = (
-            numpy.exp(predicted_training_y_values) /
-            (1 + numpy.exp(predicted_training_y_values))
-        )
-        training_error = -numpy.mean(
-            training_y_values * numpy.log2(predicted_training_y_values) +
-            (1. - training_y_values) *
-            numpy.log2(1. - predicted_training_y_values)
-        )
+        predicted_training_y_values = _inverse_logit(
+            predicted_training_y_values)
+        training_error = _binary_xentropy(
+            actual_values=training_y_values,
+            predicted_values=predicted_training_y_values)
+        training_title_string = '{0:s} ... training x-entropy = {1:.4e}'.format(
+            base_title_string, training_error)
     else:
-        training_error = numpy.mean(
-            (training_y_values - predicted_training_y_values) ** 2)
+        training_error = _mean_squared_error(
+            actual_values=training_y_values,
+            predicted_values=predicted_training_y_values)
+        training_title_string = '{0:s} ... training MSE = {1:.4e}'.format(
+            base_title_string, training_error)
 
-    training_title_string = (
-        'Iterations = {0:d} ... learning rate = {1:.4e} ... training error = '
-        '{2:.4e}'
-    ).format(num_iterations, learning_rate, training_error)
-    training_figure_file_name = (
-        '{0:s}/training_num-iterations={1:d}_learning-rate={2:f}_logistic={3:d}'
-        '.jpg'
-    ).format(output_dir_name, num_iterations, learning_rate, do_logistic)
-
+    training_figure_file_name = '{0:s}/training_results.jpg'.format(
+        output_dir_name)
     _plot_results(
         x_values=training_x_values, y_values=training_y_values, weights=weights,
         do_logistic=do_logistic, title_string=training_title_string,
@@ -291,28 +345,21 @@ def _run(true_slope, true_intercept, noise_standard_deviation, num_iterations,
 
     predicted_testing_y_values = weights[0] + weights[1] * testing_x_values
     if do_logistic:
-        predicted_testing_y_values = (
-            numpy.exp(predicted_testing_y_values) /
-            (1 + numpy.exp(predicted_testing_y_values))
-        )
-        testing_error = -numpy.mean(
-            testing_y_values * numpy.log2(predicted_testing_y_values) +
-            (1. - testing_y_values) *
-            numpy.log2(1. - predicted_testing_y_values)
-        )
+        predicted_testing_y_values = _inverse_logit(predicted_testing_y_values)
+        testing_error = _binary_xentropy(
+            actual_values=testing_y_values,
+            predicted_values=predicted_testing_y_values)
+        testing_title_string = '{0:s} ... testing x-entropy = {1:.4e}'.format(
+            base_title_string, testing_error)
     else:
-        testing_error = numpy.mean(
-            (testing_y_values - predicted_testing_y_values) ** 2)
+        testing_error = _mean_squared_error(
+            actual_values=testing_y_values,
+            predicted_values=predicted_testing_y_values)
+        testing_title_string = '{0:s} ... testing MSE = {1:.4e}'.format(
+            base_title_string, testing_error)
 
-    testing_title_string = (
-        'Iterations = {0:d} ... learning rate = {1:.4e} ... testing error = '
-        '{2:.4e}'
-    ).format(num_iterations, learning_rate, testing_error)
-    testing_figure_file_name = (
-        '{0:s}/testing_num-iterations={1:d}_learning-rate={2:f}_logistic={3:d}'
-        '.jpg'
-    ).format(output_dir_name, num_iterations, learning_rate, do_logistic)
-
+    testing_figure_file_name = '{0:s}/testing_results.jpg'.format(
+        output_dir_name)
     _plot_results(
         x_values=testing_x_values, y_values=testing_y_values, weights=weights,
         do_logistic=do_logistic, title_string=testing_title_string,
@@ -329,5 +376,5 @@ if __name__ == '__main__':
             INPUT_ARG_OBJECT, NOISE_STDEV_ARG_NAME),
         num_iterations=getattr(INPUT_ARG_OBJECT, NUM_ITERATIONS_ARG_NAME),
         learning_rate=getattr(INPUT_ARG_OBJECT, LEARNING_RATE_ARG_NAME),
-        do_logistic=getattr(INPUT_ARG_OBJECT, LOGISTIC_ARG_NAME),
+        do_logistic=bool(getattr(INPUT_ARG_OBJECT, LOGISTIC_ARG_NAME)),
         output_dir_name=getattr(INPUT_ARG_OBJECT, OUTPUT_DIR_ARG_NAME))
